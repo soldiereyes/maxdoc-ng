fa9e29144c4eae697d71ec92d1fd13171f4456ff
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-12-06 15:11:13 -0300

    feat: adiciona a possibilidade de estilizações assincronas

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import {environment} from './environments/environments';
 import {provideHttpClient} from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 
 if (environment.production) {
@@ -10,5 +11,5 @@ if (environment.production) {
 }
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient()],
+  providers: [provideHttpClient(), provideAnimationsAsync()],
 }).catch((err) => console.error(err));
