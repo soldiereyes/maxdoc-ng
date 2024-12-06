e2b7478bff9d4e840f17d65f62f672df5547c116
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-12-06 11:24:19 -0300

    feat: implementa bootstrap na main

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {environment} from './environments/environments';
+import {provideHttpClient} from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [provideHttpClient()],
+}).catch((err) => console.error(err));
