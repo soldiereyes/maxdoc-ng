d31814247bb7c7a47f009b3ef09b29a8c559fe38
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-12-06 11:23:24 -0300

    feat: cria um form para criação dos Documentos

---
+++ src/app/components/document-form/document-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-form',
+  imports: [],
+  templateUrl: './document-form.component.html',
+  styleUrl: './document-form.component.css'
+})
+export class DocumentFormComponent {
+
+}
