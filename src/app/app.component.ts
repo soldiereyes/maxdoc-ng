5eeafd5b9eaa451b157ed2cf334ad0b5171cee32
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-12-06 10:44:53 -0300

    feat: transforma o componente em standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {DocumentListComponent} from './components/document-list/document-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DocumentListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
 })
 export class AppComponent {
   title = 'maxdoc-ng';
